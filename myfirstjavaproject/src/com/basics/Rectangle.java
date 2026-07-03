9b99f7e6360b1388cd58e339ddbb6c9a2b3ffcf3
notreallymani <manikantaboddu5@gmail.com>
2026-07-03 10:46:25 +0530

    rectangle project

---
+++ myfirstjavaproject/src/com/basics/Rectangle.java
@@ -0,0 +1,17 @@
+package com.basics;
+
+public class Rectangle {
+	 void calculatearea() {
+		int length=20;
+		int breadth=23;
+		int area=length*breadth;
+		System.out.println("Area of Rectangle:"+area);
+	}
+
+	public static void main(String[] args) {
+		Rectangle r =new Rectangle();
+	r.calculatearea();	
+
+	}
+
+}

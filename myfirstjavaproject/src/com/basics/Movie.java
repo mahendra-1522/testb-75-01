79638cf33e9f1af7c802d9f93dc04253f0528f2d
notreallymani <manikantaboddu5@gmail.com>
2026-07-08 10:55:36 +0530

    movie

---
+++ myfirstjavaproject/src/com/basics/Movie.java
@@ -0,0 +1,25 @@
+package com.basics;
+
+public class Movie {
+
+	public static void main(String[] args) {
+		int id=987976;
+		byte screen=3;
+		float price =485.65f;
+		boolean status=true;
+		byte age=22;
+		byte notickets=6;
+		long contact=9876543210L;
+		float gst=13.67f;
+		
+		System.out.println("ticket id: "+id);
+		System.out.println("Screen Number: "+screen);
+		System.out.println("Ticket price: "+price);
+		System.out.println("ticket status: "+status);
+		System.out.println("customer age: "+age);
+		System.out.println("No.of.Tickets Booking: "+notickets);
+		System.out.println("Contact Number: "+contact);
+		System.out.println("GST of ticket price: "+gst);
+	}
+
+}

0f8712327b4e38e78a7e802c5bb80d5a98fb3b86
notreallymani <manikantaboddu5@gmail.com>
2026-07-28 14:49:57 +0530

    copy constructor

--- myfirstjavaproject/src/com/basics/Tractor.java
+++ myfirstjavaproject/src/com/basics/Tractor.java
@@ -1,31 +1,35 @@
 package com.basics;
 
-public class Tractor {
+public class Tractor{
 	
 	String name;
 	int model;
 	int capacity;
 	double price;
 	String feautres;
+	static Tractor t;
 	
 	Tractor(){
-		this("Tractor");
+		this("mak");
 	}
 	Tractor(String name){
-		//this(t,2000);
+	this(t,name,2000);
 		this.name=name;
 	}
-	Tractor(Tractor t,int model){
+	Tractor(Tractor t,String n,int model){
+		this(t,t.name,model,43,24335);
 		this.name=t.name;
+		
 		this.model=model;
 	}
-	Tractor(Tractor t,int capacity,double price){
+	Tractor(Tractor t,String n,int model,int capacity,double price){
+		this(t,t.name,model,capacity,price,"hard");
 		this.name=t.name;
 		this.model=t.model;
 		this.capacity=capacity;
 		this.price=price;
 	}
-	Tractor(Tractor t,String feautres){
+	Tractor(Tractor t,String n,int model,int capacity,double price,String feautres){
 		this.name=t.name;
 		this.model=t.model;
 		this.capacity=t.capacity;
@@ -42,15 +46,14 @@ public class Tractor {
 		System.out.println("******************************");
 	}
 	public static void main(String[] args) {
-		Tractor t=new Tractor();
-		t.details();
+		
 		Tractor t1=new Tractor("Mahindra");
 		t1.details();
-		Tractor t2=new Tractor(t1,2018);
+		Tractor t2=new Tractor(t1,t1.name,2018);
 		t2.details();
-		Tractor t3=new Tractor(t2,40,1999999.99);
+		Tractor t3=new Tractor(t2,t2.name,t2.model,40,1999999.99);
 		t3.details();
-		Tractor t4=new Tractor(t3,"harevester");
+		Tractor t4=new Tractor(t3,t3.name,t3.model,t3.capacity,t3.price,"harevester");
 		t4.details();
 
 	}
